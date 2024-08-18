ebe6d950a74f402d3669041a52620273887a68e8
pmendez- <paulamendezsv@gmail.com>
2024-08-18 19:16:10 +0200

    cambiado de orden el main y quitado los close fd que no necesito

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,23 +6,11 @@
 
 void close_all_fds(t_pipex *pipex)
 {
-	printf("Entra en close_all_fds --\n");
-
-	//Comprobar si el fichero de entrada esta cerrado
-	if (pipex->fd[0] != -1)
-		close_fd(&pipex->fd[0], "pipex->fd[0]");
-
-	//Comprobar si el pipe del padre esta cerrado
 	if (pipex->pipe_father[0] != -1)
 		close_fd(&pipex->pipe_father[0], "pipex->pipe_father[0]");
 
-	//Comprobar si el pipe del padre esta cerrado
 	if (pipex->pipe_father[1] != -1)
 		close_fd(&pipex->pipe_father[1], "pipex->pipe_father[1]");
-
-	//Comprobar si el fichero de salida esta cerrado
-	if (pipex->fd[1] != -1)
-		close_fd(&pipex->fd[1], "pipex->fd[1]");
 }
 
 int main(int argc, char *argv[], char **envp)
@@ -33,8 +21,8 @@ int main(int argc, char *argv[], char **envp)
 	status = 0;
     initialize_pipex(&pipex, envp, argc);
     command(&pipex, argv, argc);
+	close_all_fds(&pipex);
 	if (WEXITSTATUS(status) == 127)
 		exit(127);
-	close_all_fds(&pipex);
 	return (0);
 }
