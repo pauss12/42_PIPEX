c39ff6fff3203f6461b8061493cac475f09d9341
pmendez- <paulamendezsv@gmail.com>
2024-08-18 21:48:20 +0200

    cambiadao nombre funcion

--- src/pipex.c
+++ src/pipex.c
@@ -15,9 +15,9 @@
 void	end_process(t_pipex pipex)
 {
 	if (close(pipex.pipe_fd[1]) == -1)
-		print_error_no_cmd(RED "Error\n" END "Error closing pipe_fd[1]\n", 1);
+		error_no_cmd(RED "Error\n" END "Error closing pipe_fd[1]\n", 1);
 	if (close(pipex.pipe_fd[0]) == -1)
-		print_error_no_cmd(RED "Error\n" END "Error closing pipe_fd[0]\n", 1);
+		error_no_cmd(RED "Error\n" END "Error closing pipe_fd[0]\n", 1);
 }
 
 static void	initialize(t_pipex *pipex)
@@ -37,10 +37,10 @@ int	main(int argc, char **argv, char **envp)
 	int		status;
 
 	if (argc != 5)
-		print_error_no_cmd(RED "Error\n" END "Wrong number of arguments\n", 1);
+		error_no_cmd(RED "Error\n" END "Wrong number of arguments\n", 1);
 	initialize(&pipex);
 	if (pipe(pipex.pipe_fd) == -1)
-		print_error_no_cmd(RED "Error\n" END "Error creating pipe\n", 1);
+		error_no_cmd(RED "Error\n" END "Error creating pipe\n", 1);
 	pipex.pid = fork();
 	if (pipex.pid == 0)
 		first_command(pipex, argv[2], envp, argv[1]);
